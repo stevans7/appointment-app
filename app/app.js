65ffbdfbf5b1fa808559d6f94011839ba43ab517
stevan7 <revazaonzala@gmail.com>
2025-11-06 14:59:01 +0100

    add: .env

--- app/app.js
+++ app/app.js
@@ -1,3 +1,8 @@
-// Export the app for testing
+require('dotenv').config();  // Charge les variables d'environnement
+
+console.log('MAIL_USER:', process.env.MAIL_USER);
+console.log('MAIL_PASS:', process.env.MAIL_PASS ? '***' : 'not set');
+
 const app = require('./index');
+
 module.exports = app;
--- app/index.js
+++ app/index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+
+
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
